e9597531ef07de50d3a208b036b0d51827e9b980
abhishekjadhav347 <abhishekjadhav3470@gmail.com>
2024-07-31 15:08:51 +0530

    launch

--- create_ec2.py
+++ create_ec2.py
@@ -1,1 +1,13 @@
+import boto3
+import boto3.session
 
+aws_management_console = boto3.session.Session(profile_name="admin")
+
+ec2_console = aws_management_console.client('ec2')
+
+response = ec2_console.run_instances(
+    ImageId='ami-068e0f1a600cd311c',
+    InstanceType='t2.micro',
+    MaxCount=1,
+    MinCount=1,
+)
